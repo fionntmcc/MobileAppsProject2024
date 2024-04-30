055b81b38f1ebf5a9655f347c6a235caeb84df50
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-04-30 20:59:33 +0100

    Details page now has a button which directs the user to the official movie website using the capacitor browser plugin

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,21 +1,23 @@
 import { Component, Input, OnInit, WritableSignal, inject, signal } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonItem, } from '@ionic/angular/standalone';
+import { RouterLink, RouterLinkWithHref } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonButtons, IonButton, IonBackButton, IonItem, IonNav, IonAvatar, } from '@ionic/angular/standalone';
 import { MovieService } from 'src/services/movie.service';
 import { MovieResult } from 'src/services/interfaces';
 import { CurrencyPipe, DatePipe} from '@angular/common';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonItem, DatePipe, CurrencyPipe, ]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonButtons, IonButton, IonBackButton, IonItem, DatePipe, CurrencyPipe, IonNav, IonAvatar, RouterLinkWithHref, ]
 })
 export class DetailsPage implements OnInit {
-
   private movieService = inject(MovieService);
   public imageBaseUrl = "https://image.tmdb.org/t/p";
   public movie:WritableSignal<MovieResult | null> = signal(null);
+  public homepage:string = "";
 
   @Input()
   set id(movieId:string) {
@@ -23,8 +25,13 @@ export class DetailsPage implements OnInit {
       console.log(movie);
 
       this.movie.set(movie);
+      this.homepage = movie.homepage;
     });
 
+  }
+
+  async openMovieWebsite() {
+    await Browser.open({ url: this.homepage});
   }
 
   constructor() { }
