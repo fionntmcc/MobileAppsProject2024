72d5b036cd260742aae1787843851cff50c171f2
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-04-30 20:29:49 +0100

    Home page now has infinite scrolling, loading more movies. If movies are loading, a fake skeleton list is displayed.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,23 @@
 import { Component, inject, } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonAvatar, IonLabel, IonBadge, } from '@ionic/angular/standalone';
+import { IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  InfiniteScrollCustomEvent,
+  SearchbarCustomEvent,
+  IonList, 
+  IonItem,
+  IonLabel,
+  IonAvatar,
+  IonSkeletonText, 
+  IonAlert,
+  IonBadge,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonSearchbar,
+  IonButton,
+  IonButtons,
+ } from '@ionic/angular/standalone';
 import { MovieService } from 'src/services/movie.service';
 import { finalize, catchError} from 'rxjs';
 import { RouterLinkWithHref } from '@angular/router';
@@ -12,7 +30,7 @@ import { DatePipe } from '@angular/common';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonAvatar, IonLabel, IonBadge, DatePipe, RouterLinkWithHref, ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonAvatar, IonSkeletonText, IonAlert, IonBadge, RouterLinkWithHref, DatePipe, IonInfiniteScroll, IonInfiniteScrollContent, IonSearchbar, IonButton, IonButtons, ],
 })
 export class HomePage {
 
@@ -20,18 +38,60 @@ export class HomePage {
   private currentPage:number = 1;
   public movies:MovieResult[] = [];
   public imageBaseUrl = "https://image.tmdb.org/t/p";
+  public error = null;
+  public isLoading:boolean = false;
+  public index = 1;
+  public fakeArray = new Array(5);
+
 
   constructor() {
-    this.loadMovies(this.currentPage);
+    this.loadMovies();
     console.log(this.movies);
   }
 
-  loadMovies(page:number) {
-    this.movieService.getTopCharts(page).subscribe({
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    if (!event) {
+      this.isLoading = true;
+    }
+
+    this.movieService.getTopCharts(this.currentPage).pipe(
+      finalize(() => {
+        this.isLoading = false;
+        if (event) {
+          event.target.complete();
+        }
+      }),
+      catchError((e) => {
+        console.log(e);
+        this.error = e.error.status_message;
+        return [];
+      })  
+    )
+    .subscribe({
       next: (res) => {
         console.log(res);
         this.movies.push(...res.results);
+        if (event) {
+          event.target.disabled = res.total_pages === this.currentPage;
+        }
       },
     });
+
+    this.movieService.getTopCharts().subscribe((movies) => {
+      console.log(movies);
+    });
+  }
+
+  getMovieFromId(id:string):void {
+    this.movieService.getMovieDetails(id).subscribe((movie) => {
+      console.log(movie);
+    });
+  }
+
+  loadMoreMovies(event?: InfiniteScrollCustomEvent) {
+    this.currentPage++;
+    this.loadMovies(event);
   }
 }
