b683213562c746c9f63bef761a37bab678a9aa48
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-05-06 14:52:00 +0100

    Movies now display in order of user's rating on library page.

--- src/app/library/library.page.ts
+++ src/app/library/library.page.ts
@@ -1,27 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    InfiniteScrollCustomEvent,
-    SearchbarCustomEvent,
-    IonList, 
-    IonItem,
-    IonLabel,
-    IonAvatar,
-    IonSkeletonText, 
-    IonAlert,
-    IonBadge,
-    IonInfiniteScroll,
-    IonInfiniteScrollContent,
-    IonSearchbar,
-    IonButton,
-    IonButtons,
-    IonBackButton,
-   } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  InfiniteScrollCustomEvent,
+  SearchbarCustomEvent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAvatar,
+  IonSkeletonText,
+  IonAlert,
+  IonBadge,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonSearchbar,
+  IonButton,
+  IonButtons,
+  IonBackButton,
+} from '@ionic/angular/standalone';
 //import { DatePipe } from '@angular/common';
 import { MovieService } from 'src/services/movie.service';
-import { finalize, catchError} from 'rxjs';
+import { finalize, catchError } from 'rxjs';
 import { MovieResult, WatchedMovie } from 'src/services/interfaces';
 import { RouterLinkWithHref } from '@angular/router';
 import { DatePipe } from '@angular/common';
@@ -33,86 +34,100 @@ import { StorageService } from 'src/services/storage.service';
   templateUrl: 'library.page.html',
   styleUrls: ['library.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonAvatar, IonSkeletonText, IonAlert, IonBadge, RouterLinkWithHref, DatePipe, IonInfiniteScroll, IonInfiniteScrollContent, IonSearchbar, IonButton, IonButtons, IonBackButton, ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonAvatar, IonSkeletonText, IonAlert, IonBadge, RouterLinkWithHref, DatePipe, IonInfiniteScroll, IonInfiniteScrollContent, IonSearchbar, IonButton, IonButtons, IonBackButton,],
 })
 export class LibraryPage {
   private movieService = inject(MovieService);
   private storageService = inject(StorageService);
-  private currentPage:number = 1;
+  private currentPage: number = 1;
   public error = null;
-  public isLoading:boolean = false;
-  public isEmpty:boolean = true;
-  public movies:Array<MovieResult> = [];
-  public sortedMovies:Array<MovieResult> = [];
-  public ratings:number[] = [];
+  public isLoading: boolean = false;
+  public isEmpty: boolean = true;
+  public movies: MovieResult[] = [];
+  public ratings: number[] = [];
   public imageBaseUrl = "https://image.tmdb.org/t/p";
   public dummyArray = new Array(5);
   public index = 1;
-  public searchTerm:any = "";
-  
+  public searchTerm: any = "";
+
   constructor() {
     this.loadMovies();
     console.log(this.movies);
-    console.log(this.ratings);
   }
 
-  loadMoreMovies(event: InfiniteScrollCustomEvent) {
-    this.currentPage++;
-    //this.getMovieSearch();
-  }
-
+  // loads watched movies and sorts them by user's rating
   loadMovies(event?: InfiniteScrollCustomEvent) {
     this.error = null;
-
+    
+    /*
     if (!event) {
       this.isLoading = true;
     }
+    */
 
+    // get keys from database 
+    // (keys are movieIds)
     this.storageService.keySet()
-    .then(keys => {
-      console.log("Keyset:" + keys);
+    // once keys are returned
+      .then(keys => {
+        console.log("Keyset:" + keys);
 
-      // Below is my failed attempt to sort the movies by ratings, however it will be 
-      // easier to sort the returned array of movies than database entries
-      // responseKeys.sort((a, b) => (await this.storageService.get(a) - await this.storageService.get(b)));
-      
-      for (let i = 0; i < keys.length; i++) {
+        // Below is my failed attempt to sort the movies by ratings, however it will be 
+        // easier to sort the returned array of movies than database entries
+        // responseKeys.sort((a, b) => (await this.storageService.get(a) - await this.storageService.get(b)));
 
-        this.movieService.getMovieDetails(keys[i]).pipe(
-          catchError((e) => {
-            console.log(e);
-            this.error = e.error.status_message;
-            return [];
-          })
-        )
-        .subscribe({
-          next: (resultMovie) => {
-            this.storageService.get(keys[i])
-        .then( (res:number) => {
-          console.log(i);
-          console.log("Vote avg idx " + i + " before:" + resultMovie.vote_average);
-          this.movies[i].vote_average = res;
-          console.log("Vote avg idx " + i + " after:" + resultMovie.vote_average);
-          this.ratings.push(res);
-        })
-        .catch((e) => {
-          console.log("Error: " + e);
-        });
-        this.movies.push(resultMovie);
-          }
-        });
-      }
-      this.sortedMovies = this.movies.sort((a, b) => {
-        if (a.vote_average > b.vote_average) { return 1; }
-        return -1;
+        // loop through movieIds
+        for (let i = 0; i < keys.length; i++) {
+          // call for movieId api movie details
+          this.movieService.getMovieDetails(keys[i]).pipe(
+            // if error
+            catchError((e) => {
+              console.log(e);
+              this.error = e.error.status_message;
+              return [];
+            })
+          )
+            .subscribe({
+              // returned MovieResult
+              next: (resultMovie) => {
+                // get user's rating for movie
+                this.storageService.get(keys[i])
+                  .then((res: number) => {
+                    // set vote_average to user's rating
+                    resultMovie.vote_average = res;
+                    //push movie to array
+                    this.movies.push(resultMovie);
+
+                    // sort movies by user's rating
+                    this.movies.sort((a, b) => {
+                      return a.vote_average - b.vote_average;
+                    });
+
+                  })
+                  // if error
+                  .catch((e) => {
+                    console.log("Error: " + e);
+                  });
+              }
+            });
+        }
+        // end of for loop
+
+        // I tried sorting the movies array here like this: 
+        /*
+          this.movies.sort((a, b) => {
+            return a.vote_average - b.vote_average;
+          });
+        */
+        // However this would sort the movies based on their ratings from IMDb.
+        // I am not sure why it does this but I think it has to do with asynchronous calls.
+        // I made sure not to push resultMovie to the array prior to changing vote_average,
+        // so I am not quite sure what is causing this behaviour.
+
+      })
+      .catch(e => {
+        console.log("Error: " + e);
       });
-      console.log("movies");
-      console.log(this.sortedMovies);
-      console.log("ratings");
-    })
-    .catch(e => {
-      console.log("Error: " + e);
-    });
 
     /*
     this.movieService.getTopCharts(this.currentPage).pipe(
@@ -144,7 +159,7 @@ export class LibraryPage {
     */
   }
 
-  getSearchbarValue(event:SearchbarCustomEvent) {
+  getSearchbarValue(event: SearchbarCustomEvent) {
     this.movies = [];
     this.searchTerm = event.detail.value?.trim();
     console.log(this.searchTerm);
@@ -155,7 +170,7 @@ export class LibraryPage {
       this.isEmpty = false;
       this.getMovieSearch();
     }
-    
+
   }
 
   getMovieSearch() {
@@ -168,32 +183,32 @@ export class LibraryPage {
         catchError((e) => {
           console.log(e);
           this.error = e.error.status_message;
-        return[];
+          return [];
         })
       )
-      .subscribe({
-        next: (res) => {
-          console.log("Results" + res);
-  
-          this.movies.push(...res.results);
-          res.total_pages === this.currentPage;
-        },
-      });
-    }
-}
+        .subscribe({
+          next: (res) => {
+            console.log("Results" + res);
 
-  getMovieFromId(id:string):void {
+            this.movies.push(...res.results);
+            res.total_pages === this.currentPage;
+          },
+        });
+    }
+  }
+
+  getMovieFromId(id: string): void {
     this.movieService.getMovieDetails(id).subscribe((movie) => {
       console.log(movie);
     });
   }
 
   // Methods to interact with DB
-  async set(key:string, value:number) {
+  async set(key: string, value: number) {
     return await this.storageService.set(key, value);
   }
 
-  async get(key:string) {
+  async get(key: string) {
     return await this.storageService.get(key);
   }
 
@@ -201,7 +216,7 @@ export class LibraryPage {
     return await this.storageService.keySet();
   }
 
-  async removeWatchedMovie(id:string) {
+  async removeWatchedMovie(id: string) {
     await this.storageService.remove(id);
   }
 }
